3347120b54c777130a368d61fd1a82df39612cd9
unknown <somya4287@gmail.com>
2021-01-31 13:58:14 +0530

    Updates

---
+++ precache-manifest.4d8b4295380bf37c4bc6d48a12c69f7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea50d7ea21e2c9d23c3a071b061bed5",
+    "url": "/Food-Recipe/index.html"
+  },
+  {
+    "revision": "1bd777309e6a1d327fc5",
+    "url": "/Food-Recipe/static/css/main.88ed76e6.chunk.css"
+  },
+  {
+    "revision": "a1b45fad096453412ae2",
+    "url": "/Food-Recipe/static/js/2.a216d849.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Food-Recipe/static/js/2.a216d849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bd777309e6a1d327fc5",
+    "url": "/Food-Recipe/static/js/main.3163052b.chunk.js"
+  },
+  {
+    "revision": "1966547e31d760536b62",
+    "url": "/Food-Recipe/static/js/runtime-main.1149b18e.js"
+  },
+  {
+    "revision": "97d2ccb0c45736417da5b7b635caa64f",
+    "url": "/Food-Recipe/static/media/stwaberry.97d2ccb0.jpg"
+  }
+]);
